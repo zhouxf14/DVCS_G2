73d8d737af396556303565b449745257ab0d98ca
gavinb222 <32890830+gavinb222@users.noreply.github.com>
2019-11-13 16:33:25 -0500

    Updated repository building function
    prints errors in different parts of build_repo_structure, fixes bugs coming from me thinking in python.

--- file_processor.rb
+++ file_processor.rb
@@ -6,55 +6,69 @@ module FP
   #config file
   #
 
-  def new_folder(folder_name):
-    if (system("mkdir #{folder_name}") and system("cd #{folder_name}") and system("cd ..")):
-      True
-    else:
-      print("Error building folder #{folder_name}")
-      False
+  def new_folder(folder_name)
+    if (system("mkdir #{folder_name}") and system("cd #{folder_name}") and system("cd .."))
+      true
+    else
+      puts("Error building folder #{folder_name}")
+      false
     end
   end
 
-  def new_file(filename):
-    if (system("touch #{filename}")):
-      True
-    else:
-      print("Error building file #{filename}")
-      False
+  def new_file(filename)
+    if (system("touch #{filename}"))
+      true
+    else
+      puts("Error building file #{filename}")
+      false
     end
   end
 
   def append_text(text,filename)
-    if (system("\"#{text}\" >> #{filename}")):
-      True
-    else:
-      print("Error appendint text to #{filename}")
-      False
+    if (system("\"#{text}\" >> #{filename}"))
+      true
+    else
+      puts("Error appending text to #{filename}")
+      false
     end
   end
 
   #check whether or not folder 'query' exists in the current directory
-  def check_folder_contents(query):
-    if (system("[ -d #{query} ]")):
-      print("Error in current directory: #{query} already exists.")
-      False
-    else:
-      True
+  def check_folder_contents(query)
+    if (system("[ -d #{query} ]"))
+      puts("Error in current directory: #{query} already exists.")
+      false
+    else
+      true
     end
   end
 
   #once we know that we can init files and folders, build the repo with empty folders and blank files.
-  def build_repo_structure():
-    new_folder("hooks")
-    new_folder("info")
-    new_folder("objects")
-    new_folder("refs")
-    new_file("config")
-    new_file("description")
-    new_file("HEAD")
-    new_file("index")
+  def build_repo_structure()
+    #if there isn't a dvcs folder
+    if check_folder_contents("dvcs")
+      #build and enter the dvcs folder
+      if (new_folder("dvcs") and system("cd dvcs"))
+        #build folders inside the dvcs folder
+        if (new_folder("hooks") and new_folder("info") and new_folder("objects") and new_folder("refs"))
+          #build files inside the dvcs folder
+          if (new_file("config") and new_file("description") and new_file("HEAD") and new_file("index"))
+            #if everything works, navigate back out, and return success.
+            system("cd ..")
+            true
+          end
+        else
+          puts("error building folders")
+          false
+        end
+      else
+        puts("error building dvcs folder")
+        false
+      end
+    else
+      puts("Error: dvcs folder already exists")
+      false
+    end
   end
-
-
 
 end

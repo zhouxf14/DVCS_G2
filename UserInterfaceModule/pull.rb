06d1b4af6739323636730b828f3cf834ddedbc44
zhouxf14 <zhouxf14@163.com>
2019-12-08 18:19:04 -0500

    part of pull implemented

--- UserInterfaceModule/pull.rb
+++ UserInterfaceModule/pull.rb
@@ -3,6 +3,17 @@ require_relative '../FileProcessorModule/file_system'
 
 module Pull
   def Pull.pull_return_back(remote_location)
-  	if(DataStructure.getHEAD() == )
+  	local_head = DataStructure.getHEAD()
+  	local_status = DataStructure.status
+  	if (local_status.include? "Not Committed" || local_status.include? "Uncommitted Changes" || local_status.include? "Deleted") {
+  		puts "todo"
+  	} else {
+  		FileSystem.get_remote_repo(remote_location, Dir.pwd)
+  		if (local_head == DataStructure.getHEAD()) {
+  			puts "It's already the latest version"
+  		} else {
+  			puts "The latest version has been pulled successfully"
+  		}
+  	}
   end
  end
